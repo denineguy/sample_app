6d90cc31b8ba94c9275a70d9d0b08efc383753d8
Denine Guy <denineguy@gmail.com>
2014-09-04 23:23:47 -0400

    change session#new to an form_tag vs form_fo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
       # Sign the user in and redirect to the user's show page.
       sign_in user
       redirect_to user
@@ -22,3 +22,22 @@ class SessionsController < ApplicationController
     redirect_to root_url
   end
 end
+
+
+# def create
+#     #find user with email
+#     @user = User.find_by(:email => params[:email])
+#     if @user && @user.authenticate(params[:password])
+#       session[:user_id] = @user.id  #This represents being logged in
+#       redirect_to root_path
+#     else
+#       flash.now[:notice] = "Invalid email and/or password "
+#       render :new
+#     end
+#   end
+ 
+
+#   def destroy
+#     reset_session
+#     redirect_to root_path # maybe root path
+#   end 
